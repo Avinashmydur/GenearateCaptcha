20a90b63a89ebea189f2018b6caf30c8c577f4da
Avinash <Avinash@LAPTOP-HAIAHBJF>
2024-08-05 18:30:00 +0530

    Add project files.

---
+++ GenearateCaptcha/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using CaptchaMvc.HtmlHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenearateCaptcha.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string empty)
+        {
+            if (this.IsCaptchaValid("captch is not valid"))
+            {
+                return RedirectToAction("ThankyouPage");
+            }
+            ViewBag.ErrMessage = "Error:Captch is not valid.";
+            return View();
+        }
+        public ActionResult ThankyouPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ GenearateCaptcha/Models/FeedBack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenearateCaptcha.Models
+{
+    public class FeedBack
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+    }
+}
